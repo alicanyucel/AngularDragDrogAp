40cf8932b6721719bb7ce01c9d839cd068566263
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-19 18:20:33 +0300

    AngularDragDrog bitti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { moveItemInArray } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DragDrogAp';
+  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+  onDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
 }
